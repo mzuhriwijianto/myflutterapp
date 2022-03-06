165c1a17090e848eb54ead2ad5038ffbf95e2730
M. Zuhri Wijianto <zuhrywijianto@gmail.com>
2022-03-06 14:11:16 +0700

    First commit flutter app

---
+++ lib/helloworld.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void helloworld() {
+  runApp(const MyApp());
+}
+
+<div class="greybox">
+<div class="redbox">
+Lorem ipsum
+</div>
+</div>
+
+    .greybox {
+background-color: #e0e0e0; /* grey 300 */
+width: 320px;
+height: 240px;
+font: 900 24px Roboto;
+position: relative;
+}
+    .redbox {
+background-color: #ef5350; /* red 400 */
+padding: 16px;
+color: #ffffff;
+position: absolute;
+top: 24px;
+left: 24px;
+}
