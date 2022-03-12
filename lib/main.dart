74f3f21a90d39719b98152f33c9f84dd14e29762
zuhri <zuhrywijianto@gmail.com>
2022-03-12 20:36:34 +0800

    Bilangan Prima

--- lib/main.dart
+++ lib/main.dart
@@ -47,52 +47,190 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 3121552808;
+// class _MyHomePageState extends State<MyHomePage> { //Menampilkan bilangan 1 sampai dengan 10
+//   int _counter = 1;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//       if(_counter>10){
+//         _counter = 1;
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Hello World',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> { //Menampilkan bilangan Ganjil Atau Genap
+//   int _counter = 1;
+//   String _text = "Ganjil";
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//       if(_counter>10){
+//         _counter = 1;
+//       }
+//
+//       if(_counter%2 == 0){
+//         _text = "Genap";
+//       }else{
+//         _text = "Ganjil";
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Hello World',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             Text(
+//               _text,
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> { //Menampilkan bilangan Ganjil mulai dari 1 sampai n
+//   int _counter = 1;
+//   String _text = "Ganjil";
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//       if(_counter>10){
+//         _counter = 0;
+//       }
+//       _text = "Ganjil: ";
+//       for(int i=0; i<=_counter; i++){
+//         if(i%2 != 0){
+//           _text += '${i}, ';
+//         }
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Hello World',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             Text(
+//               _text,
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+class _MyHomePageState extends State<MyHomePage> { //Menampilkan bilangan Prima
+  int _counter = 0;
+  String _text = "Ganjil";
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      if(_counter>10){
+        _counter++;
+      }
+      _text = "Prima: ";
+      int x, y, z;
+
+      if (_counter > 100){
+        _counter = 1;
+        }
+
+      for (x = 2; x <= _counter; x++){
+        y = 0;
+        for (z = 2; z < x; z++){
+          if (x % z == 0){
+            y = 1;
+          }
+        }
+        if (y == 0){
+          _text += "${x}, ";
+        }
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -102,6 +240,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text(
+              _text,
+              style: Theme.of(context).textTheme.headline4,
+            ),
           ],
         ),
       ),
@@ -109,7 +251,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
